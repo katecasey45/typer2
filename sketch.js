dc1b10a966b97cfddb9b2d4745ec34fc558e07d8
KateC <kec22@fsu.edu>
2025-03-19 14:29:55 -0400

    no more chat

--- sketch.js
+++ sketch.js
@@ -24,15 +24,11 @@ function setup() {
 function draw() {
     background(255);
 
-    // Check if idle time has passed and we should start displaying beads
     if (millis() - lastTypedTime > idleTime) {
-        if (!isDisplayingBeads) {
-            console.log("Starting to display beads...");
+
             isDisplayingBeads = true;
         }
-    }
-
-    // Only display beads if the flag is true
+  
     if (isDisplayingBeads) {
         displayBeads();
     }
@@ -57,7 +53,7 @@ function sendText() {
     let text = sentence;
     if (socket.readyState == 1) {
         socket.send(text);
-        console.log("Sent:", text);
+        console.log("Sent:" + text);
     } else {
         console.log("Socket not ready.");
     }
@@ -69,7 +65,6 @@ function openHandler() {
 
 function messageHandler(event) {
     var msg = event.data;
-    console.log("Received message: ", msg);
 
     if (msg != 0) {
         console.log("Received sentiment value: " + msg);
@@ -92,19 +87,14 @@ function createBeadsBasedOnSentiment(score) {
     }
 
     beads.push(bead);
-    console.log("Bead created: ", bead);
-    // Do not reset beadIndex here, it should be controlled by draw
+    beadIndex = beads.length - 1;
+    letterIndex = 0;
+
 }
 
 function displayBeads() {
     fill(0);
-    console.log("Bead index: ", beadIndex, "Letter index: ", letterIndex);
-
-    // Ensure we're within the bounds of the beads array
-    if (beadIndex < beads.length) {
-        let currentBead = beads[beadIndex];
-
-        // Increment letterIndex every few frames
+   
         if (frameCount % displaySpeed === 0 && letterIndex < currentBead.length) {
             letterIndex++;
         }
@@ -115,17 +105,13 @@ function displayBeads() {
             text(currentBead, width / 2, height / 2 + (i * 30));
         }
 
-        // Draw the current bead (in progress)
+        let currentBead = beads[beadIndex]
         let displayString = currentBead.substring(0, letterIndex);
         text(displayString, width / 2, height / 2 + (beadIndex * 30));
 
-        // Once the current bead is fully displayed, move to the next bead
         if (letterIndex >= currentBead.length) {
             beadIndex++;
             letterIndex = 0;
         }
-    } else {
-        // Stop displaying beads once all are processed
-        isDisplayingBeads = false;
+
     }
-}
