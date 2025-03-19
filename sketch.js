7c8a6c805567dcf64f5c8aa4858b6ecd04dcee52
KateC <kec22@fsu.edu>
2025-03-19 14:15:05 -0400

    more log checks version test

--- sketch.js
+++ sketch.js
@@ -1,82 +1,81 @@
 let sentence = '';
-let beads = [];
-let lastTypedTime = 0;
-let idleTime = 10000;
-let isDisplayingBeads = false;
-let beadIndex = 0;
-let letterIndex = 0;
-let displaySpeed = 5;
-
-var host = '127.0.0.1:8080';
-var socket;
-
-function setup() {
-    createCanvas(600, 400);
-    textSize(20);
-    textAlign(CENTER, CENTER);
-    textFont("Courier New");
-
-    socket = new WebSocket('ws://' + host);
-    socket.onopen = openHandler;
-    socket.onmessage = messageHandler;
-}
-
-function draw() {
-    console.log("Drawing...");
-    background(255);
-
-    if (millis() - lastTypedTime > idleTime) {
-        isDisplayingBeads = true;
-    }
-
-    if (isDisplayingBeads) {
-        displayBeads();
-    }
-}
-
-function keyTyped() {
-    if (key === '.' || key === '?' || key === '!') {
-        if (sentence.length > 0) {
-            let bead = sendText();
-            beads.push(bead);
-            sentence = '';
-            lastTypedTime = millis();
-            isDisplayingBeads = false;
-        }
-    } else {
-        sentence += key;
-        lastTypedTime = millis();
-    }
-}
-
-function sendText() {
-    let text = sentence;
-
-    if (socket.readyState === WebSocket.OPEN) {
-        socket.send(text);
-        console.log("Sent: " + text);
-    } else {
-        console.log("Socket not ready. Retrying...");
-        setTimeout(() => sendText(), 1000);
-    }
-}
-
-function openHandler() {
-    console.log("Connected to socket server at " + host);
-}
-
-function messageHandler(event) {
-    var msg = event.data;
-    console.log("Received message: ", msg);
-
-    if (msg != 0) {
-        console.log("Received sentiment value: " + msg);
-        createBeadsBasedOnSentiment(parseFloat(msg));
-    } else {
-        console.log("That statement came back as neutral  ╰(*°▽°*)╯");
-        createBeadsBasedOnSentiment(0);
-    }
-}
+ let beads = [];
+ let lastTypedTime = 0;
+ let idleTime = 10000;
+ let isDisplayingBeads = false;
+ let beadIndex = 0;
+ let letterIndex = 0;
+ let displaySpeed = 5;
+ 
+ var host = '127.0.0.1:8080';
+ var socket;
+ 
+ function setup() {
+     createCanvas(600, 400);
+     textSize(20);
+     textAlign(CENTER, CENTER);
+     textFont("Courier New");
+ 
+     socket = new WebSocket('ws://' + host);
+     socket.onopen = openHandler;
+     socket.onmessage = messageHandler;
+ }
+ 
+ function draw() {
+     background(255);
+ 
+     if (millis() - lastTypedTime > idleTime) {
+         isDisplayingBeads = true;
+     }
+     console.log("Canvas is rendering");
+ 
+     if (isDisplayingBeads) {
+         displayBeads();
+     }
+ }
+ 
+ function keyTyped() {
+     if (key === '.' || key === '?' || key === '!') {
+         if (sentence.length > 0) {
+             let bead = sendText();
+             beads.push(bead);
+             sentence = '';
+             lastTypedTime = millis();
+             isDisplayingBeads = false;
+         }
+     } else {
+         sentence += key;
+         lastTypedTime = millis();
+     }
+ }
+ 
+ function sendText() {
+     let text = sentence;
+ 
+     if (socket.readyState == 1) {
+         socket.send(text);
+         console.log("Sent: " + text);
+     } else {
+         console.log("Socket not ready.");
+     }
+ }
+ 
+ function openHandler() {
+     console.log("Connected to socket server at " + host);
+ }
+ 
+ function messageHandler(event) {
+     var msg = event.data;
+     console.log("Received message: ", msg);
+ 
+     if (msg != 0) {
+         console.log("Received sentiment value: " + msg);
+         createBeadsBasedOnSentiment(parseFloat(msg));
+     } else {
+         console.log("That statement came back as neutral  ╰(*°▽°*)╯");
+         createBeadsBasedOnSentiment(0);
+     }
+ }
 
 function createBeadsBasedOnSentiment(score) {
     let bead = '';
